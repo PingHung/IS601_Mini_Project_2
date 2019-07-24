8bff10cfcc5ba1ef346887c67dfb0226e8cb2020
ping <ph49@njit.edu>
2019-07-24 11:37:27 -0400

    Task: Create a home page

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.default')
+
+@section('content')
+
+
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-3">Ping Hung Ho</h1>
+            <p>IS601 Mini Project 2</p>
+            <p>This is the homepage</p>
+        </div>
+    </div>
+
+
+@endsection
