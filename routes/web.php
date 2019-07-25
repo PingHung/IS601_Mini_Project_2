13ac83ba82ef2fe2025454aa22f899ce2f17c532
ping <ph49@njit.edu>
2019-07-25 16:14:32 -0400

    8.  Create a controller, route, and view to show an about page and add it to the main menu of your site.

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Auth::routes();
 Route::get('/profile', 'HomeController@profile')->name('profile');
 
 Route::get('/', 'PagesController@home')->name('home');
+Route::get('/about', 'PagesController@about')->name('about');
 Route::get('/contact', 'PagesController@contact')->name('contact');
 Route::post('/contact', 'PagesController@store')->name('contact.store');
 Route::get('/thanks/{name}', 'PagesController@thanks')->name('thanks');
